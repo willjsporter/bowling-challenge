548b0e7ccbfda59f139c4cd8d7d7db122fcd3fe3
Will Porter <willjsporter@hotmail.co.uk>
2017-09-09 00:45:14 +0100

    bowling logic working

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,30 +5,53 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("should be able to store the score from each go in an array", function() {
-    game.bowl(1);
-    game.bowl(8);
-    game.bowl(9);
-    game.bowl(0);
-    expect(game.framelist).toEqual([[1,8],[9,0]]);
+  describe("#framelist keeps track of frames", function() {
+
+    it("should be able to store the score from each go in an array", function() {
+      game.bowl(1);
+      game.bowl(8);
+      game.bowl(9);
+      game.bowl(0);
+      expect(game.framelist).toEqual([[1,8],[9,0]]);
+    });
+
+    it("should detect a strike and skip to the next go", function() {
+      game.bowl(1);
+      game.bowl(8);
+      game.bowl(10);
+      game.bowl(1);
+      expect(game.framelist).toEqual([[1,8],[10,null],[1,null]]);
+    });
   });
 
-  it("should detect a strike and skip to the next go", function() {
-    game.bowl(1);
-    game.bowl(8);
-    game.bowl(10);
-    game.bowl(1);
-    expect(game.framelist).toEqual([[1,8],[10,null],[1,null]]);
-  });
+  describe("#scorecard updates at the right times", function() {
 
-  it("should update scorecard each roll when there are no strikes or spares", function() {
-    game.bowl(1);
-    expect(game.scorecard[0]).toEqual(1)
-    game.bowl(8);
-    expect(game.scorecard[0]).toEqual(9)
-    game.bowl(9);
-    expect(game.scorecard[0]).toEqual(9)
-    expect(game.scorecard[1]).toEqual(18);
+    it("#updates each roll when there are no strikes or spares", function() {
+      game.bowl(1);
+      expect(typeof(game.scorecard[0])).toEqual('undefined')
+      game.bowl(8);
+      expect(game.scorecard[0]).toEqual(9)
+      game.bowl(9);
+      game.bowl(0);
+      expect(game.scorecard[1]).toEqual(18);
+    });
+
+    it("#updates after one more roll when there is a spare", function() {
+      game.bowl(1);
+      game.bowl(9);
+      expect(typeof(game.scorecard[0])).toEqual('undefined')
+    });
+
+    it("#updates after two more rolls when there is a strike", function() {
+      game.bowl(10);
+      game.bowl(10);
+      game.bowl(2);
+      game.bowl(3);
+      expect(game.scorecard[0]).toEqual(22);
+      expect(game.scorecard[1]).toEqual(37);
+      expect(game.scorecard[2]).toEqual(42);
+      expect(typeof(game.scorecard[3])).toBe('undefined');
+    });
   });
 
   describe("should prevent the player from bowling at the end of the tenth frame", function() {
@@ -49,6 +72,46 @@ describe("Game", function() {
       // expect(try {game.bowl(1)}; catch(err) {return err}).toBe(null);
       game.bowl(1);
       expect(function(){game.bowl(0)}).toThrowError("Game is over, no more bowling!");
+    });
+  });
+
+  describe("the framelist is updated after strikes", function() {
+
+    beforeEach(function() {
+      for(i=0;i<3; i++){game.bowl(10)}
+    });
+
+    it("registers the next scores in the framelist following a series of strikes", function() {
+      game.bowl(2);
+      game.bowl(8);
+      game.bowl(6);
+      game.bowl(3);
+      expect(game.framelist.length).toEqual(5);
+      expect(game.framelist).toEqual([[10,null],[10,null],[10,null],[2,8],[6,3]]);
+      expect(game.scorecard).toEqual([30,52,72,88,97]);
+    });
+  });
+  describe("the framelist is updated after spares", function() {
+
+    beforeEach(function() {
+      for(i=0;i<2; i++){game.bowl(6);game.bowl(4)}
+    });
+
+    it("registers the next scores in the framelist following a series of strikes", function() {
+      console.log(game.framelist); console.log(game.scorecard);
+      game.bowl(10);
+      console.log(game.framelist); console.log(game.scorecard);
+      game.bowl(8);
+      console.log(game.framelist); console.log(game.scorecard);
+      game.bowl(2);
+      console.log(game.framelist); console.log(game.scorecard);
+      game.bowl(2);
+      console.log(game.framelist); console.log(game.scorecard);
+      game.bowl(3);
+      console.log(game.framelist); console.log(game.scorecard);
+      expect(game.framelist.length).toEqual(5);
+      expect(game.framelist).toEqual([[6,4],[6,4],[10,null],[8,2],[2,3]]);
+      expect(game.scorecard).toEqual([16,36,56,68,73]);
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -1,18 +1,38 @@
 function Game() {
   this.score = 0;
-  this.scorecard = []; //1d
-  this.framelist=[]; //2d
+  this.scorecard = []; //1d - should only update when score confirmed
+  this.framelist=[]; //2d - updates every throw
   this.allowedToRoll = true
+  this.last3 = [null,null,null];
 };
 
 Game.prototype.bowl = function(value){
+
+//breaks if the game is over
   if(!this.allowedToRoll){throw new Error("Game is over, no more bowling!")}
 
+
+//sets some variables to shorten written code
   var frameNo = this.framelist.length
   if(frameNo!==0){
     var notStrike = (this.framelist.last()[0]!==10);
     var secondGo = (this.framelist.last()[1]==null);
   }
+
+//updates the last three rolls
+this.last3=[this.last3[1],this.last3[2],value]
+
+//sorts out scoring for previous strikes and spares with the first condition catching issues early in the game
+if(this.last3[0]===null){}
+else if(this.last3[0] === 10) {
+  this.scorecard.push(this.scorecard.last()+10+value+this.last3[1]
+)}
+else if(this.last3[0]+this.last3[1]===10&&this.framelist.last().last()!==null) {
+  this.scorecard.push(this.scorecard.last()+10+value
+)};
+//condition in second else if prevents accidentally recording a spare
+//if the previous two goes add to 10 but are from different frames
+
 
   if(frameNo===0){
   this.framelist.push([value,null]);
@@ -37,9 +57,17 @@ Game.prototype.bowl = function(value){
     this.allowedToRoll=false
   }
 //either last one is [x,null] or [x,y] at this stage. Want to update if it's [x,y] and push new frame if it's [x,null]
-  if(this.scorecard.length===10){this.scorecard[10]+=value;}
-  else if(this.framelist.last()[1]!==null){this.scorecard[this.scorecard.length-1] += value;}
-  else if(this.framelist.last()[1]===null){this.scorecard.push(this.scorecard.last()+value);}
+  // if(this.scorecard.length===10){this.scorecard[10]+=value;}
+  // else if(this.framelist.last()[1]!==null){this.scorecard[this.scorecard.length-1] += value;}
+  // else if(this.framelist.last()[1]===null){this.scorecard.push(this.scorecard.last()+value);}
+
+  //scorecard should be updated at end of frame if no spare or strike, otherwise, wait until score is confirmed
+  // if(this.framelist.length>0){
+  if(this.framelist.last().last()===null){}
+  else if(this.framelist.last()[0]+this.framelist.last()[1]<10){
+    if(this.scorecard.length===0){this.scorecard.push(value+this.framelist[0][0]);}
+    else{this.scorecard.push(value+this.framelist.last()[0]+this.scorecard.last());}
+  }
 
 };
 
